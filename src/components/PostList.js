1c569a1fad804c721af275869c58382e9445a61a
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-07-22 17:42:16 -0600

    created a renderList function that maps the list of posts

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -3,16 +3,31 @@ import { connect } from 'react-redux'
 import { fetchPosts } from '../actions'
 
 class PostList extends React.Component {
-
   componentDidMount() {
     this.props.fetchPosts()
+  }
+
+  renderList() {
+    return this.props.posts.map(post => {
+      return (
+        <div className="item" key={post.id}>
+          <i className="large middle aligned icon user" />
+          <div className="content">
+            <div className="description">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        </div>
+      )
+    })
   }
 
   render() {
     // console.log("posts", this.props.posts)
     return (
-      <div>
-        Post List
+      <div className="ui relaxed divided list">
+        {this.renderList()}
     </div>
     )
   }
