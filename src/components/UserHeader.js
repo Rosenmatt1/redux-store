f4dffab494a6098f224996ca83f4527d612bbfef
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-07-22 18:23:59 -0600

    Added userHeader reducer

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -8,10 +8,18 @@ class UserHeader extends React.Component {
   }
 
   render() {
+    const user = this.props.users.find(user => user.id === this.props.userId)
+
+    if (!user) return null
+    
     return (
-      <div>User Header</div>
+      <div className="header"> {user.name} </div>
     )
   }
 }
 
-export default connect(null, {fetchUser} )(UserHeader)
+const mapStateToProps = (state) => {
+  return { users: state.users}
+}
+
+export default connect(mapStateToProps, { fetchUser } )(UserHeader)
