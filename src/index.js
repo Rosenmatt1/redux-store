4d6677b0a25c97a1d6e1558839448164f97911a2
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-07-24 11:04:02 -0600

    Added redux dev tools to index.js

--- src/index.js
+++ src/index.js
@@ -6,15 +6,14 @@ import App from './components/App';
 import reducers from './reducers';
 import thunk from 'redux-thunk';
 
-const composeEnhancers = window.__REDUX__DEVTOOLS_EXTENSION_COMPOSE__
-const store = createStore(reducers, applyMiddleware(thunk))
-
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>
   , document.querySelector('#root')
-);
+)
 
 
