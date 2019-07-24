f68f60047ddbc2dc6d02cae125ebf28b34b9ec3c
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-07-23 23:10:09 -0600

    added a lodash uniq map to get userIds in fetchPostsAndUsers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,12 @@
-// import _ from 'lodash'
+import _ from 'lodash'
 import jsonPlaceholder from '../apis/jsonPlaceholder'
 
-export const fetchPostsAndUsers = () => async dispatch => {
-  // console.log("about to fetch posts")
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  console.log("about to fetch posts")
   await dispatch(fetchPosts())
-  // console.log("fethcedPosts")
+
+  const userIds = _.uniq(_.map(getState().posts, 'userId'))
+  console.log(userIds)
 }
 
 export const fetchPosts = () => async dispatch => {
