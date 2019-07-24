b09fd8610d7db9089f9c7a1ecade51731b69cbab
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-07-23 20:28:20 -0600

    Memoized the fetchUser function with the lodash memoize

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,24 +1,31 @@
+import _ from 'lodash'
 import jsonPlaceholder from '../apis/jsonPlaceholder'
 
-export const fetchPosts  = () => async dispatch => {
-    const response = await jsonPlaceholder.get('/posts')
-    dispatch({ 
-      type: 'FETCH_POSTS', 
-      payload: response.data
-    })
-    // return {
-    //   type: 'FETCH_POSTS',
-    //   payload: reponse
-    // }
-  }
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonPlaceholder.get('/posts')
+  dispatch({
+    type: 'FETCH_POSTS',
+    payload: response.data
+  })
+  // return {
+  //   type: 'FETCH_POSTS',
+  //   payload: reponse
+  // }
+}
 
-export const fetchUser = (id) => async dispatch => {
+export const fetchUser = id => dispatch => {
+  _fetchUser(id, dispatch)
+}
+
+const _fetchUser = _.memoize(async(id, dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${id}`)
   dispatch({
     type: 'FETCH_USER',
     payload: response.data
   })
-}
+})
+
+
 
 
 //Not needed with redux thunk
