2c5414c95f0348a0ba5fb2cf2a7cc82d8124c764
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-08-12 22:12:34 -0600

    Added a test and the test suite has 1 test passing

--- src/components/_tests_/form.test.js
+++ src/components/_tests_/form.test.js
@@ -1,4 +1,10 @@
 import React from 'react'
 // import App from '../../App'
-import Navigation from '../Navigation'
-import { shallow } from 'enzyme'
+// import Navigation from '../Navigation'
+// import { shallow } from 'enzyme'
+
+
+it('my first test', () => {
+  const isTrue = true
+  expect(isTrue).toBe(true)
+})
