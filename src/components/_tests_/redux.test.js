c2c4060d1fa096616bba706c62534a4f7e0ad5a4
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-08-17 15:05:46 -0600

    Added some example tests

--- src/components/_tests_/redux.test.js
+++ src/components/_tests_/redux.test.js
@@ -1,7 +1,16 @@
-import { clearTitle } from '../actions'
+import { clearTitle } from '../../actions'
 
 it('return an empty string as payload', () => {
-  const action = clearTitle()
-  console.log(action)
+  const action = clearTitle().action.payload
+  console.log("action", action)
   expect(action).toBe("")
 })
+
+
+// const { enzymeWrapper, props } = shallowSetup();
+// // enzymeWrapper.find(selector) : Find every node in the render tree that matches the provided selector. 
+// expect(enzymeWrapper.find('img').hasClass('ui image')).toBe(true);
+// expect(enzymeWrapper.find('.header').text()).toBe(props.title);
+// expect(enzymeWrapper.find('button.ui.basic.red.button').text()).toBe('Delete');
+// // enzymeWrapper.containsMatchingElement(node i.e reactElement) : Check if the provided React element matches one element in the render tree. Returns a boolean.
+// expect(enzymeWrapper.containsMatchingElement(<button>Delete</button>)).toBe(true);
